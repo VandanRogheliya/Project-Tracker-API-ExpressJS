4c2ab10ba72f9392048de0146e05021d1bffd917
VandanRogheliya <vandan@daolens.com>
2023-04-09 12:04:59 +0530

    remove render support

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ var app = express();
 
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
@@ -68,7 +68,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({ err });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 // for testing purposes
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project-T API' });
+router.get("/", function (req, res, next) {
+  res.json({ title: "Project-T API" });
 });
 
 module.exports = router;
